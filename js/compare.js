56449873ed64bfe78d02c68fdfcb7eefa9557ed5
l4ySS <90033305+l4ySS@users.noreply.github.com>
2023-04-07 18:08:06 +0300

    Add files via upload

---
+++ js/compare.js
@@ -0,0 +1,4 @@
+function compare(object){
+    for(let iter in object)
+    console.log(iter);
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+
+
+compare(json/example.json);
