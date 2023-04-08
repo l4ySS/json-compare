15fcf54e347c0c1989245665c6b2b59fe38908f8
l4ySS <90033305+l4ySS@users.noreply.github.com>
2023-04-08 20:22:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,29 +2,30 @@ var res;
 var obj1;
 var obj2;
 var diffKeys = [];
-var diffValues= new Map();
-function isDeepEqual(object1, object2){
-    const objKeys1 = Object.keys(object1);
-    const objKeys2 = Object.keys(object2);
-    let isEqual = true;
+var diffValues= [];
 
-    if (objKeys1.length !== objKeys2.length) return false;
+function isDeepEqual(object1, object2){
+    let objKeys1 = Object.keys(object1);
+    let objKeys2 = Object.keys(object2);
+    let isEqual = true;
+  //if (objKeys1.length !== objKeys2.length) return false;
     
     for (var key of objKeys1) {
-      const value1 = object1[key];
-      const value2 = object2[key];
-  
-      const isObjects = isObject(value1) && isObject(value2);
+      if(!objKeys2.includes(key)) {
+        diffKeys.push(key);
+        break;
+      }
+      let value1 = object1[key];
+      let value2 = object2[key];
+      
+      let isObjects = isObject(value1) && isObject(value2);
   
       if ((isObjects && !isDeepEqual(value1, value2))
       ) {
-        diffKeys.push(key);
         isEqual=false;
       }
       else if ((!isObjects && value1 !== value2)){
-        diffKeys.push(key); ;
-        diffValues.set(key, value1);
-     
+        diffValues.push(value1);
         isEqual=false;
       }
     }
@@ -43,23 +44,28 @@ function mapToObj(map){
   }
 
 function start(){
-    console.log(isDeepEqual(obj1, obj2));
-    console.log(diffKeys);
-    
+    diffKeys = [];
+    diffValues = [];
+
+    document.getElementById('result3').value = " ";
+    document.getElementById('result4').value = " ";
     let str = JSON.stringify(obj1, null, " "); 
     let pre = document.getElementById('result1')
-    pre.innerText = str;
+    pre.innerText = str; 
 
     str = JSON.stringify(obj2, null, " "); 
     pre = document.getElementById('result2')
     pre.innerText = str;
 
+    if (!isDeepEqual(obj1, obj2)){
     pre = document.getElementById('result3')
     pre.innerText = JSON.stringify(diffKeys);
-
     pre = document.getElementById('result4')
-    pre.innerText = JSON.stringify(mapToObj(diffValues), null, " ");
-    console.log(mapToObj(diffValues));
+    pre.innerText = JSON.stringify(diffValues, null, " ");
+    }
+    else {
+      document.getElementById('result3').innerText = "Файлы одинаковы."
+    }
 }
 
 function inputJSON() {
@@ -73,7 +79,7 @@ function inputJSON() {
         res = event.target.result;
         obj1 = JSON.parse(res);
     };
-    reader1.readAsText(file1);
+    reader1.readAsTex (file1);
     
     reader2.onload = function(event) {
         res = event.target.result;
